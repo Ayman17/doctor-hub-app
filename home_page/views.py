d5dd51fa2f6cde8bfb4adb50b603133986dbc518
Ayman17 <77356245+Ayman17@users.noreply.github.com>
2024-09-03 07:04:48 +0300

    Display doctors spec. and areas in home page

--- home_page/views.py
+++ home_page/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'home_page/index.html')
+    return redirect('getDoctorsData')
 
 def about(request):
     return render(request, 'home_page/about.html')
